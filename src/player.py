b80c69ff01d30dbf1e3995f5a26b6b5d7766807b
Robert Benavidez <murabitbenavidez@gmail.com>
2020-08-13 20:37:28 -0400

    player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = []
+
+    def __str__(self):
+        return f"Player: {self.name} Current Room: {self.current_room} Player Inventory: {self.inventory}"
+
+    def move(self, room):
+        self.current_room = room
+
+    def take_item(self, item):
+        self.inventory.append(item)
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
